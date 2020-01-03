8ed5ae67ff5f618b9831fb9ba95d619ac1d1b64a
Tao PR <pataoengineer@gmail.com>
2020-01-03 11:32:51 +0100

    setup test base

--- thundercats/src/test/scala/com/tao/thundercats/physical/DataTest.scala
+++ thundercats/src/test/scala/com/tao/thundercats/physical/DataTest.scala
@@ -15,10 +15,36 @@ import com.tao.thundercats.base.{SparkTestInstance, SparkStreamTestInstance}
 import org.scalatest._
 import Matchers._
 
+object IO {
+  def getFile(path: String): Option[File] = {
+    val f = new File(path)
+    if (f.exists) Some(f) else None
+  }
+
+  def delete(file: File): Option[Boolean] = Some(file.delete)
+
+  def deleteFiles(files: List[String]) = {
+    files.foreach{ f => 
+        for {
+          w <- IO.getFile(f);
+          _ <- IO.delete(w) }
+          yield true
+      }
+  }
+}
+
 class DataSuite extends FunSpec with Matchers with SparkStreamTestInstance {
 
   describe("Basic IO"){
+    lazy val tempCSV = File.createTempFile("tc-test-", ".csv").getAbsolutePath
 
+    it("PRE: cleanup tempfiles"){
+      IO.deleteFiles(tempCSV :: Nil)
+    }
+
+    it("POST: cleanup tempfiles"){
+      IO.deleteFiles(tempCSV :: Nil)
+    }
   }
 
 }
