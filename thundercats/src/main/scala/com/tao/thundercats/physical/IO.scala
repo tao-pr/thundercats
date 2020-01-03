9243b3f7d089452670016ed17b736140b99bad18
Tao PR <pataoengineer@gmail.com>
2020-01-03 16:39:04 +0100

    use Option as monad instance

--- thundercats/src/main/scala/com/tao/thundercats/physical/IO.scala
+++ thundercats/src/main/scala/com/tao/thundercats/physical/IO.scala
@@ -8,54 +8,31 @@ import org.apache.spark.sql.{Encoders, Encoder}
 import org.apache.spark.sql.avro._
 import org.apache.spark.sql.functions._
 
-/**
- * Base Monad
- */
-trait M[A] {
-  def unit(a: A): M[A]
-  def flatMap(f: A => M[A]): M[A]
-}
-
-/**
- * A Monad representing any dataframe
- */
-private [physical] trait Data extends M[DataFrame]{
-  protected def read: DataFrame
-  override def unit(a: DataFrame): M[DataFrame] = DataWrap(a)
-  override def flatMap(f: DataFrame => M[DataFrame]): M[DataFrame] = {
-    unit(read)
-  }
-}
-
-/**
- * An instance of Dataframe monad
- */
-case class DataWrap(df: DataFrame) extends Data {
-  override protected def read: DataFrame = df
-  override def unit(a: DataFrame): M[DataFrame] = DataWrap(a)
-}
+import scala.util.Try
 
 object Read {
-  case class CSV[A](path: String, withHeader: Boolean = true)
-  (implicit val spark: SparkSession) extends Data {
-    override protected def read: DataFrame = {
-      import spark.implicits._
-      spark
+  def csv(path: String, withHeader: Boolean = true)
+  (implicit spark: SparkSession): Option[DataFrame] = {
+    import spark.implicits._
+    Try {
+      val df = spark
         .read
         .option("header", withHeader.toString)
         .option("inferSchema", "true")
-        .csv(path)
-    }
+        .csv(path)  
+      Some(df)
+    } getOrElse(None)
   }
   
-  case class Parquet[A](path: String) 
-  (implicit val spark: SparkSession) extends Data {
-    override protected def read: DataFrame = {
-      import spark.implicits._
-      spark
+  def parquet(path: String) 
+  (implicit spark: SparkSession): Option[DataFrame] = {
+    import spark.implicits._
+    Try {
+      val df = spark
         .read
         .parquet(path)
-    }
+      Some(df)
+    } getOrElse(None)
   }
 }
 
@@ -65,39 +42,31 @@ object Write {
   case object NoPartition extends Partition
   case class PartitionCol(cols: List[String]) extends Partition
 
-  private [physical] trait Generic {
-    val df: DataFrame
-    val partition: Partition
-    protected def preprocess = partition match {
-      case NoPartition => df.coalesce(1).write
-      case PartitionCol(cols) => df.write.partitionBy(cols:_*)
-    }
+  protected def preprocess(df: DataFrame, partition: Partition) = partition match {
+    case NoPartition => df.coalesce(1).write
+    case PartitionCol(cols) => df.write.partitionBy(cols:_*)
   }
 
-  case class CSV[A](
-    override val df: DataFrame, 
+  def csv(
+    df: DataFrame, 
     path: String,
-    override val partition: Partition = NoPartition)
-  (implicit val spark: SparkSession) extends Generic with Data {
-    override protected def read: DataFrame = {
-      import spark.implicits._
-      preprocess
-        .option("header", "true")
-        .option("inferSchema", "true")
-        .csv(path)
-      df
-    }
+    partition: Partition = NoPartition)
+  (implicit spark: SparkSession): Option[DataFrame] = {
+    import spark.implicits._
+    preprocess(df, partition)
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv(path)
+    Some(df)
   }
-  
-  case class Parquet[A](
-    override val df: DataFrame, 
+
+  def parquet(
+    df: DataFrame, 
     path: String,
-    override val partition: Partition = NoPartition)
-  (implicit val spark: SparkSession) extends Generic with Data {
-    override protected def read: DataFrame = {
-      import spark.implicits._
-      preprocess.parquet(path)
-      df
-    }
+    partition: Partition = NoPartition)
+  (implicit spark: SparkSession): Option[DataFrame] = {
+    import spark.implicits._
+    preprocess(df, partition).parquet(path)
+    Some(df)
   }
 }
--- thundercats/src/test/scala/com/tao/thundercats/physical/DataTest.scala
+++ thundercats/src/test/scala/com/tao/thundercats/physical/DataTest.scala
@@ -11,6 +11,7 @@ import sys.process._
 import scala.reflect.io.Directory
 
 import com.tao.thundercats.base.{SparkTestInstance, SparkStreamTestInstance}
+import com.tao.thundercats.physical._
 
 import org.scalatest._
 import Matchers._
@@ -25,21 +26,39 @@ object IO {
 
   def deleteFiles(files: List[String]) = {
     files.foreach{ f => 
-        for {
-          w <- IO.getFile(f);
-          _ <- IO.delete(w) }
-          yield true
-      }
+      for {
+        w <- IO.getFile(f);
+        _ <- IO.delete(w) }
+        yield true
+    }
   }
 }
+
+case class A(i: Int, s: Option[String])
 
 class DataSuite extends FunSpec with Matchers with SparkStreamTestInstance {
 
   describe("Basic IO"){
+    import spark.implicits._
     lazy val tempCSV = File.createTempFile("tc-test-", ".csv").getAbsolutePath
+
+    lazy val df = List(
+      A(1, None),
+      A(2, Some("foo")),
+      A(3, Some("bar"))
+    ).toDS.toDF
 
     it("PRE: cleanup tempfiles"){
       IO.deleteFiles(tempCSV :: Nil)
+    }
+
+    it("write and read csv"){
+      val dfRead = for { 
+        b <- Write.csv(df, tempCSV)
+        c <- Read.csv(tempCSV)
+      } yield c
+
+      // TAOTODO
     }
 
     it("POST: cleanup tempfiles"){
