7eac37b2e031e7951f32a43251ed64bea091e5d7
starcolon <tao@starcolon.com>
2020-02-15 19:40:32 +0100

    remove unused code, rename features to pipe

--- thundercats/src/main/scala/com/tao/thundercats/model/Pipe.scala
+++ thundercats/src/main/scala/com/tao/thundercats/model/Pipe.scala
@@ -23,42 +23,6 @@ import com.tao.thundercats.physical._
 import com.tao.thundercats.functional._
 import com.tao.thundercats.physical.Implicits._
 
-object Feature {
-
-  /** 
-   * DataFrame with tagged columns
-   */
-  case class TaggedDataFrame(
-    df: DataFrame,
-    featureCols: Set[Column], 
-    targetCol: Column, 
-    labelCol: Column)
-
-  /**
-   * All non-labeled numerical columns, or array of numerical columns 
-   * will become features
-   */
-  def fromDF(df: DataFrame, targetCol: String, labelCol: String): MayFail[TaggedDataFrame] = MayFail {
-    val featureCols = df.columns.toSet -- Set(targetCol, labelCol)
-    val schemaMap = df.schemaMap
-    val numFeatureCols = featureCols
-      .map{ c => schemaMap(c) match {
-        case IntegerType => Some(col(c))
-        case DoubleType => Some(col(c))
-        case FloatType => Some(col(c))
-        case ArrayType(IntegerType,_) => Some(col(c))
-        case ArrayType(DoubleType,_) => Some(col(c))
-        case ArrayType(FloatType,_) => Some(col(c))
-        case _ => None
-      }}.flatten
-    
-    TaggedDataFrame(df, numFeatureCols, col(targetCol), col(labelCol))
-  }
-
-  def evaluate(pipe: Pipeline, taggedDf: TaggedDataFrame, output: AnyVal): MayFail[TaggedDataFrame] = ???
-  def selection(pipe: Pipeline, taggedDf: TaggedDataFrame, top: Int=10, metric: AnyVal): MayFail[TaggedDataFrame] = ???
-}
-
 object Pipe {
 
   def join(pipes: Pipeline*): MayFail[Pipeline] = MayFail {
