c16dfec93ec911767a4c8ad3559371f89d22f17d
starcolon <tao@starcolon.com>
2020-02-16 02:31:33 +0100

    initial feature module

---
+++ thundercats/src/main/scala/com/tao/thundercats/model/Feature.scala
@@ -0,0 +1,43 @@
+package com.tao.thundercats.model
+
+import org.apache.spark.sql.{Dataset, DataFrame}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Column,Row}
+import org.apache.spark.sql.catalyst.encoders._
+import org.apache.spark.sql.{Encoders, Encoder}
+import org.apache.spark.sql.avro._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+import org.apache.spark.ml.feature.{HashingTF, IDF, Tokenizer, VectorAssembler}
+import org.apache.spark.ml.{Transformer, PipelineModel}
+import org.apache.spark.ml.{Pipeline, Estimator, PipelineStage}
+import org.apache.spark.ml.tuning.CrossValidatorModel
+
+import java.io.File
+import sys.process._
+import scala.reflect.io.Directory
+import scala.util.Try
+
+import com.tao.thundercats.physical._
+import com.tao.thundercats.functional._
+import com.tao.thundercats.physical.Implicits._
+
+/**
+ * Collection of basic transformer generators
+ */
+object Feature {
+
+  def encodeStrings(df: DataFrame, ignoreColumns: Set[String]=Set.empty): PipelineStage = {
+    val blocks = df
+      .schema
+      .toList.collect{ 
+        case StructField(colName,StringType,_,_) if !ignoreColumns.contains(colName) => 
+          new HashingTF().setInputCol(colName).setOutputCol(colName)
+      }
+
+    new Pipeline().setStages(blocks.toArray)
+  }
+
+  def scaleNumbers(df: DataFrame, scale: Column=lit(1.0), logScale: Boolean=false, ignoreColumns: Set[String]=Set.empty): PipelineStage = ???
+}
