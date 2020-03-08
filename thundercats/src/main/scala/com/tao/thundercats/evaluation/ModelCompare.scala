cceaab19c7d3d74476dbe37475a5872025459ca7
Tao PR <pataoengineer@gmail.com>
2020-03-08 18:00:52 +0100

    initial model compare module

--- thundercats/src/main/scala/com/tao/thundercats/evaluation/ModelCompare.scala
+++ thundercats/src/main/scala/com/tao/thundercats/evaluation/ModelCompare.scala
@@ -12,11 +12,15 @@ import org.apache.spark.sql.types._
 import org.apache.spark.ml.feature.{HashingTF, Tokenizer, VectorAssembler}
 import org.apache.spark.ml.{Transformer, PipelineModel}
 import org.apache.spark.ml.{Pipeline, Estimator, PipelineStage}
+import org.apache.spark.ml.{Predictor}
 import org.apache.spark.ml.tuning.CrossValidatorModel
 import org.apache.spark.ml.param._
 import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.mllib.stat.correlation.ExposedPearsonCorrelation
+import org.apache.spark.rdd.DoubleRDDFunctions
 
 import java.io.File
+import java.lang.IllegalArgumentException
 import sys.process._
 import scala.reflect.io.Directory
 import scala.util.Try
@@ -27,19 +31,8 @@ import com.tao.thundercats.physical.Implicits._
 import com.tao.thundercats.estimator._
 
 /**
- * Base significance / score of model or individual feature
+ * Comparison between two predictors
+ * A represents the base model score
+ * B represents the contender model score
  */
-trait Significance[T <: Metric] {
-  def measure(df: DataFrame, model: PipelineModel): Metric
-}
-
-/**
- * Base metric to measure significance level
- */
-trait Metric {
-  /**
-   * Returns true if this metric is better than another
-   */
-  def > (b: Metric): Boolean
-}
-
+trait ModelCompare[A <: Score, B <: Score] {}
