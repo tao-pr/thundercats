5e830fe3c080a85120cfdb7ba6c2619725950f4c
starcolon <tao@starcolon.com>
2020-05-17 15:44:02 +0200

    feature compare vector

---
+++ thundercats/src/main/scala/com/tao/thundercats/evaluation/Base.scala
@@ -0,0 +1,12 @@
+package com.tao.thundercats.evaluation
+
+import org.apache.spark.sql.{Dataset, DataFrame}
+
+import com.tao.thundercats.physical._
+import com.tao.thundercats.functional._
+import com.tao.thundercats.physical.Implicits._
+import com.tao.thundercats.estimator._
+
+private [evaluation] trait BaseMeasure[A] {
+  def % (df: DataFrame, specimen: Specimen): MayFail[A]
+}
--- thundercats/src/main/scala/com/tao/thundercats/model/Preset.scala
+++ thundercats/src/main/scala/com/tao/thundercats/model/Preset.scala
@@ -1,8 +1,8 @@
 package com.tao.thundercats.model
 
-import org.apache.spark.ml.Pipeline
-import org.apache.spark.mllib.classification.LogisticRegression
-import import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.{Pipeline, PipelineStage}
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.feature.VectorAssembler
 
 import com.tao.thundercats.evaluation._
 
@@ -10,14 +10,26 @@ import com.tao.thundercats.evaluation._
  * Preset of simple estimators
  */
 object Preset {
-  def linearReg(features: FeatureColumn, labelCol: String, outputCol: String,
+  def linearReg(
+    features: FeatureColumn, 
+    labelCol: String, 
+    outputCol: String,
     maxIters: Int = 10) = {
+    val (featureCol, initPipes): (String, Array[PipelineStage]) = features match {
+      case Feature(c) => (c, Array.empty)
+      case AssemblyFeature(arr,featCol) => 
+        ("features", Array(
+          new VectorAssembler()
+            .setInputCols(arr.toArray)
+            .setOutputCol(featCol)))
+    }
     new Pipeline().setStages(
+      initPipes ++ Array(
       new LogisticRegression()
-        .setFeaturesCol(features.asArray)
+        .setFeaturesCol(featureCol)
         .setPredictionCol(outputCol)
         .setLabelCol(labelCol)
-        .setMaxIter(maxIters)
+        .setMaxIter(maxIters))
     )
   }
 }
