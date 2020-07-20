32b5fda9cac95a60b2eabe8e43fd7b1d43cf43d8
starcolon <tao@starcolon.com>
2020-07-20 23:38:33 +0200

    test model compare

--- thundercats/src/main/scala/com/tao/thundercats/model/Preset.scala
+++ thundercats/src/main/scala/com/tao/thundercats/model/Preset.scala
@@ -10,14 +10,22 @@ import com.tao.thundercats.evaluation._
  * Preset of simple estimators
  */
 object Preset {
+
+  // Simple linear regression
   def linearReg(
     features: FeatureColumn, 
     labelCol: String, 
     outputCol: String,
-    maxIters: Int = 10) =  
-      new Pipeline().setStages(Array(new LinearRegression()
+    maxIters: Int = 10,
+    elasticNetParam: Option[Double] = None) =  {
+      val lg = new LinearRegression()
         .setFeaturesCol(features.colName)
         .setPredictionCol(outputCol)
         .setLabelCol(labelCol)
-        .setMaxIter(maxIters)))
+        .setMaxIter(maxIters)
+      new Pipeline().setStages(Array(elasticNetParam match {
+        case None => lg
+        case Some(p) => lg.setElasticNetParam(p)
+      }))
+    }
 }
