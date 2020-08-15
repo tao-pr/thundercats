71b6165b562257529908f65664e9c49e6e3e7d5b
Tao PR <pataoengineer@gmail.com>
2020-08-16 00:24:18 +0200

    initial config module and empty resource files

--- samples/src/main/scala/com/tao/thundercats/samples/Main.scala
+++ samples/src/main/scala/com/tao/thundercats/samples/Main.scala
@@ -1,3 +1,13 @@
+package com.tao.thundercats.samples
+
+import com.tao.thundercats.samples.base._
+
 /**
  * Main entry point
  */
+
+object Main extends App {
+
+  Console.println("HELLO!")
+  Console.println(s"Reading sample csv : ${Data.pathSampleCSV}")
+}
---
+++ samples/src/main/scala/com/tao/thundercats/samples/base/Config.scala
@@ -0,0 +1,8 @@
+package com.tao.thundercats.samples.base
+
+object Data {
+
+  val pathSampleCSV = getClass.getResource("/inventory.csv").getPath
+  val pathSampleParquet = getClass.getResource("/sales.parquet").getPath
+
+}
