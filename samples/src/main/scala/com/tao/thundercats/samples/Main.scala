2c54ee109ae0f4efce6f16f910a1e5c4a5cd3968
Tao PR <pataoengineer@gmail.com>
2020-08-18 00:38:10 +0200

    wip sample

--- samples/src/main/scala/com/tao/thundercats/samples/Main.scala
+++ samples/src/main/scala/com/tao/thundercats/samples/Main.scala
@@ -2,6 +2,7 @@ package com.tao.thundercats.samples
 
 import com.tao.thundercats.samples.base._
 import com.tao.thundercats.physical._
+import com.tao.thundercats.functional.MayFail
 
 /**
  * Main entry point
@@ -11,11 +12,25 @@ object Main extends SparkBase {
 
   override val name = "samples"
 
-  Console.println("HELLO!")
-  val pipeInput = Read.csv(Data.pathCityTempCSV)
+  Console.println("Starting samples!")
+  val pipeInput = for {
+    // Columns : Region,Country,State,City,Month,Day,Year,AvgTemperature
+    cityTemp <- Read.csv(Data.pathCityTempCSV)
+    noise <- Fake.genNoise
+  } yield cityTemp
 
 
 
-  Console.println("SHUTTING DOWN!")
-  stop()
+
+  Console.println("Shutting down kernel!")
+  end()
+
+  Console.println("Ending")
+}
+
+object Fake {
+  def genNoise() = MayFail {
+    // TAOTODO
+    ???
+  }
 }
