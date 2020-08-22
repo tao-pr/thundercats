ef2aac45f5d94a7f3fa2a29b648e3f28dcb4beee
Tao PR <pataoengineer@gmail.com>
2020-08-23 01:05:40 +0200

    add sbt assembly plugin

--- samples/src/main/scala/com/tao/thundercats/samples/subapp/DataPipeline.scala
+++ samples/src/main/scala/com/tao/thundercats/samples/subapp/DataPipeline.scala
@@ -4,16 +4,16 @@ import com.tao.thundercats.samples.base._
 import com.tao.thundercats.physical._
 import com.tao.thundercats.functional.MayFail
 
+import org.apache.spark.sql.SparkSession
+
 object DataPipeline extends BaseApp {
 
-  override def runMe(args: Seq[String]) = {
+  override def runMe(implicit spark: SparkSession) = {
     val pipeInput = for {
       // Columns : Region,Country,State,City,Month,Day,Year,AvgTemperature
       cityTemp <- Read.csv(Data.pathCityTempCSV)
-      noise <- Fake.genNoise
+      _ <- Screen.showDF(cityTemp, Some("cityTemp (CSV)"))
     } yield cityTemp
-
-    
 
 
 
