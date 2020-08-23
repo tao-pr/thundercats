d55cbefb0a43f7874beade298e897b344bda1569
Tao PR <pataoengineer@gmail.com>
2020-08-23 21:40:34 +0200

    read example

--- samples/src/main/scala/com/tao/thundercats/samples/base/Config.scala
+++ samples/src/main/scala/com/tao/thundercats/samples/base/Config.scala
@@ -2,6 +2,13 @@ package com.tao.thundercats.samples.base
 
 object Data {
 
-  val pathCityTempCSV = getClass.getResource("/city_temperature.csv").getPath
+  def pathCityTempCSV(rootPath: String) = getPath("/city_temperature.csv", rootPath)
+  def pathCountryCSV(rootPath: String) = getPath("/countries-of-the-world.csv", rootPath)
+
+  private def getPath(file: String, rootPath: String = "") = 
+    if (rootPath.isEmpty)
+      getClass.getResource(file).getPath
+    else
+      s"${rootPath}/data/${file}"
 
 }
