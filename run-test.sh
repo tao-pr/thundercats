6cb793ff456e4be2afa72feb6c770d9590f63b6e
Tao PR <pataoengineer@gmail.com>
2021-12-11 09:43:41 +0100

    run-test now injects dynamodb correctly

--- run-test.sh
+++ run-test.sh
@@ -16,20 +16,36 @@ aws dynamodb create-table \
     --attribute-definitions \
         AttributeName=Item,AttributeType=S \
         AttributeName=Value,AttributeType=N \
-        AttributeName=Note,AttributeType=S \
     --key-schema \
-      AttributeName=Item,KeyType=HASH AttributeName=Value,KeyType=RANGE \
-    --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
+      AttributeName=Item,KeyType=HASH \
+      AttributeName=Value,KeyType=RANGE \
+    --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
+    --endpoint-url http://localhost:8000 > /dev/null
 
 echo "... Injecting sample records"
 aws dynamodb put-item \
     --table-name Entry \
-    --item \
-        '{"Item": {"S": "First Entry"}, "Value": {"N": 0}, "Note": {"S": "Unknown"}}' \
-        '{"Item": {"S": "Second Entry"}, "Value": {"N": 1}, "Note": {"S": "Unknown"}}' \
-        '{"Item": {"S": "Third Entry"}, "Value": {"N": 2}, "Note": {"S": "Unknown"}}' \
-        '{"Item": {"S": "Forth Entry"}, "Value": {"N": 0}, "Note": {"S": ""}}' \
-    --return-consumed-capacity TOTAL
+    --item '{"Item": {"S": "First Entry"}, "Value": {"N": "0"}, "Note": {"S": "Unknown"}}' \
+    --return-consumed-capacity TOTAL \
+    --endpoint-url http://localhost:8000 > /dev/null
+
+aws dynamodb put-item \
+    --table-name Entry \
+    --item '{"Item": {"S": "Second Entry"}, "Value": {"N": "1"}, "Note": {"S": "Unknown"}}' \
+    --return-consumed-capacity TOTAL \
+    --endpoint-url http://localhost:8000 > /dev/null
+
+aws dynamodb put-item \
+    --table-name Entry \
+    --item '{"Item": {"S": "Third Entry"}, "Value": {"N": "2"}, "Note": {"S": "Unknown"}}' \
+    --return-consumed-capacity TOTAL \
+    --endpoint-url http://localhost:8000 > /dev/null
+
+aws dynamodb put-item \
+    --table-name Entry \
+    --item '{"Item": {"S": "Forth Entry"}, "Value": {"N": "0"}, "Note": {"S": ""}}' \
+    --return-consumed-capacity TOTAL \
+    --endpoint-url http://localhost:8000 > /dev/null  
 
 echo "[DONE]"
 
